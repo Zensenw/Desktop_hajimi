26d271057cb0c32b8eb79fd701f372441ee611b3
zsw <wanzisen2009@163.com>
2025-08-26 14:26:26 +0800

    8.26 2

--- Scripts/new_script.gd
+++ Scripts/new_script.gd
@@ -1,1 +1,10 @@
 extends Node
+func _ready():
+	var timer = Timer.new()
+	timer.wait_time = 1.0
+	timer.timeout.connect(_on_timer_timeout)
+	add_child(timer)
+	timer.start()
+
+func _on_timer_timeout():
+	print("helloworld")
